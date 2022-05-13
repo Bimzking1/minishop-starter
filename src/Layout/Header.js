092fa3bc51ef8987f0f7eba4ae7aafd95721d84a
Bimzking1 <kingofbimo@gmail.com>
2022-05-13 13:15:46 +0700

    Done

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import jwtDecode from 'jwt-decode'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+
+    const user = useSelector( store => store.user.data)
 
     return (
         <div className="bg-green-700 text-white" >
@@ -12,11 +16,16 @@ const Header = () => {
                     </div>
                     <div>
                         <ul className="flex space-x-4">
+                            {/* ALL */}
                             <li><Link to="/products">Products</Link></li>
                             <li><Link to="/shopping-cart">Cart</Link></li>
-                            <li><Link to="/order-history">My Order</Link></li>
-                            <li><Link to="/login">Login</Link></li>
+                            {/* PUBLIC ONLY */}
+                            { user === null && <li><Link to="/login">Login</Link></li>}
+                            {/* PROTECTED */}
+                            { user !== null && <li><Link to="/order-history">My Order</Link></li> }
+                            { user !== null && <li><Link to="/logout">Logout</Link></li> }
                         </ul>
+                        { user !== null && <div>{ user.email }</div>  }
                     </div>
                 </div>
             </div>
--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,19 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import userSlice from '../store/user'
+import jwtDecode from 'jwt-decode'
 import { useForm } from 'react-hook-form'
 
 const Login = () => {
 
     const { register, handleSubmit, formState } = useForm()
+    const [loginStatus, setLoginStatus] = useState({
+        succes: false,
+        message: ''
+    })
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const formSubmitHandler = (data) => {
-        // proses login
+        const postData = {
+            email: data.user_email,
+            password: data.user_password
+        }
+        axios.post('http://localhost:4000/login', postData)
+        .then( res => {
+            if (typeof res.data.accessToken !== 'undefined') {
+                // Menyimpan token di localstorage
+                localStorage.setItem('minishopAccessToken', res.data.accessToken)
+                // Menyimpan redux di store
+                const user = jwtDecode(res.data.accessToken)
+                axios.get(`http://localhost:4000/users/${user.sub}`)
+                .then( res => {
+                    dispatch( userSlice.actions.addUser({ userData: res.data }))
+                    navigate('/')
+                })
+            }
+        }) .catch( err => {
+            setLoginStatus({
+                success: false,
+                message: 'Sorry, something is wrong. Try again later.'
+            })
+        })
     }
 
   return (
     <section>
             <div className="container py-8">
                 <div className="max-w-[500px] mx-auto">
+                    { ( !loginStatus.succes && loginStatus.message ) && <p className="text-sm text-red-500 italic">{loginStatus.message}</p>}
                     <form onSubmit={ handleSubmit(formSubmitHandler) }>
                         <div className="mb-4">
                             <label htmlFor="email">Email</label>
